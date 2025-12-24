f836ca5269130609aa715c99fa0d819576129a61
MMZL256 <138811963+MMZL256@users.noreply.github.com>
2025-12-24 16:23:30 -0500

    Update TrIAgeWebsite.py

--- TrIAgeWebsite.py
+++ TrIAgeWebsite.py
@@ -1,12 +1,23 @@
 import streamlit as st
 import pandas as pd
+import torch
+import torchvision
+from PIL import Image
+from torchvision import transforms
+
+#Image normalizer transform
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
 
 st.title("Opération TrIAge")
 st.write("Hello World!!! :)")
 
-enable = st.checkbox("Enable camera")
-wastePicture = st.camera_input("Prenez une photo de l'objet:",
+enable = st.checkbox("Activer la caméra")
+wastePicture_buffer = st.camera_input("Prenez une photo de l'objet:",
                              disabled=not enable)
 
-if wastePicture:
-    st.image(wastePicture)
+if wastePicture_buffer is not None:
+    wasteImage = transform(wastePicture_buffer)
