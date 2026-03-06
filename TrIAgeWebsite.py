4ed253ee938e1eb18f099a577b35f0d48bafb9ab
MMZL256 <138811963+MMZL256@users.noreply.github.com>
2026-03-06 18:33:50 -0500

    Update TrIAgeWebsite.py
    Rearranged UI;
    Imported new resnet34 model with larger dataset

--- TrIAgeWebsite.py
+++ TrIAgeWebsite.py
@@ -7,6 +7,7 @@ import torchvision
 from PIL import Image
 from torchvision import transforms
 from torchvision.transforms import v2
+from torchvision.models import resnet34, ResNet34_Weights
 from huggingface_hub import hf_hub_download
 from transformers import AutoTokenizer, pipeline
 import kornia
@@ -34,21 +35,45 @@ st.markdown(
 
 #LE MODÈLE
 
-#Image normalizer transform
-canny = kornia.filters.Canny(low_threshold=0.2, high_threshold=0.25)
+canny = kornia.filters.Canny(low_threshold=0.99, high_threshold=0.999)
+
 v2Transform = v2.Compose([
     v2.Resize((224, 224)),
+    #v2.RandomPhotometricDistort(brightness=(0.875,1.125),
+                                #contrast=(1, 1), 
+                                #saturation=(1, 1),
+                                #hue=(-0.02, 0.02),
+                                #p=0.5),
     v2.ToImage(),
-    v2.Grayscale(num_output_channels=1),
     v2.ToDtype(torch.float32, scale=True),
+    #v2.GaussianNoise(mean=0, sigma=0.05, clip=True),
+    #v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    #v2.Grayscale(num_output_channels=1)
 ])
 
+kTransform = nn.Sequential(
+    #kornia.geometry.transform.Resize((224, 224)),
+    #kornia.augmentation.RandomHorizontalFlip(p=0.5),
+    #kornia.augmentation.RandomAffine(degrees=10, translate=(0.05, 0.05),
+                                     #scale=(0.95, 1.05), padding_mode=2),
+    #kornia.augmentation.ColorJitter(brightness=(0.875,1.125),
+                                    #contrast=(1, 1), 
+                                    #saturation=(1, 1),
+                                    #hue=(-0.02, 0.02),
+                                    #p=0.75),
+    #kornia.augmentation.RandomGaussianNoise(mean=0, std=0.05),
+    kornia.augmentation.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    kornia.color.RgbToGrayscale()
+)
+
 def transform(img):
+    #img = v2Transform(img)
+    #imgbchw = img.unsqueeze(0)
+    #_, imgedges = canny(imgbchw)
+    #rotatedImage = kTransform(imgedges)
+    #imgchw = imgedges.squeeze(0)
     img = v2Transform(img)
-    imgbchw = img.unsqueeze(0)
-    _, imgedges = canny(imgbchw)
-    #shownEdges = kornia.tensor_to_image(imgedges.byte())
-    return imgedges.squeeze(0)
+    return img
 
 #Class names
 #classes = ("AluminumFoil", "BananaPeel", "Bottles", "Cans", "Cardboard", "Cups", "FoodWaste",
@@ -56,40 +81,37 @@ def transform(img):
 #           "PlasticBags", "Shoes", "SnackPackages", "Straws", "Styrofoam", "UsedBrownPaper",
 #           "UsedWhitePaper")
 
-classes = ("AluminumFoil", "CansLaterals", "CansTops", 
-           "Gobelets", "GobeletsLids", "JuiceBottlesLaterals", "JuiceBottlesTops",
-           "JuiceBoxLaterals", "JuiceBoxTops", "MilkBoxes", "SandwichPackage", "SnackPackages",
-           "UsedPaper", "WaterBottlesLaterals")
-frclasses = ("Papier aluminium", "Canette d'aluminium (côté)", "Canette d'aluminium (haut)", 
-             "Gobelet", "Couvercle de gobelet", "Bouteille de jus (côté)", "Bouteille de jus (haut)",
-             "Boîte de jus (côté)", "Boîte de jus (haut)", "Carton de lait", "Boîte à sandwich", 
-             "Emballage de plastique métallisé", "Papier usé", "Bouteille d'eau de plastique (côté)")
+#classes = ("AluminumFoil", "CansLaterals", "CansTops", 
+#           "Gobelets", "GobeletsLids", "JuiceBottlesLaterals", "JuiceBottlesTops",
+#           "JuiceBoxLaterals", "JuiceBoxTops", "MilkBoxes", "SandwichPackage", "SnackPackages",
+#           "UsedPaper", "WaterBottlesLaterals")
+#frclasses = ("Papier aluminium", "Canette d'aluminium (côté)", "Canette d'aluminium (haut)", 
+#             "Gobelet", "Couvercle de gobelet", "Bouteille de jus (côté)", "Bouteille de jus (haut)",
+#             "Boîte de jus (côté)", "Boîte de jus (haut)", "Carton de lait", "Boîte à sandwich", 
+#             "Emballage de plastique métallisé", "Papier usé", "Bouteille d'eau de plastique (côté)")
+classes = ("BananaPeels", "CansLaterals", "CansTops", "JuiceBottlesLaterals",
+        "JuiceBottlesTops", "JuiceBoxLaterals", "JuiceBoxTops", "MilkBoxes", 
+        "SnackPackages", "UsedPaper")
+frclasses = ("Pelure de banane", "Canette d'aluminium (côté)",
+             "Canette d'aluminium (haut)", "Bouteille de jus (côté)",
+             "Bouteille de jus (haut)", "Boîte de jus (côté)", 
+             "Boîte de jus (haut)", "Boîte de lait", "Emballage", "Papier usé")
 
 #Network architecture
 class NeuralNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes=10):
         super().__init__()
-        #Convolutional stage
-        self.conv1 = nn.Conv2d(1, 32, 5)
-        self.pool1 = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 64, 5)
-        self.pool2 = nn.MaxPool2d(2, 2)
-        #Full connections (y=Wx+b)
-        self.fc1 = nn.Linear(179776, 1024)
-        self.fc2 = nn.Linear(1024, 512)
-        self.fc3 = nn.Linear(512, 14)
+        self.resnet34 = resnet34(weights=None)
+        # Replace the final fully connected layer
+        in_features = self.resnet34.fc.in_features
+        self.resnet34.fc = nn.Linear(in_features, num_classes)
 
     def forward(self, x):
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
-        x = torch.flatten(x, start_dim=0, end_dim=-1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.resnet34(x)
         return x
 
 REPO_ID = "MMZL/TrIAge"
-FILENAME = "wasteClassTEST14.pth"
+FILENAME = "wasteClassAD_RESNET34.pth"
 
 @st.cache_resource(show_spinner="En train de charger le modèle...")
 def load_model():
@@ -110,7 +132,7 @@ def get_model():
 st.set_page_config(page_title="CSL TrIAge", page_icon="♻️", layout="wide")
 
 st.title("Opération TrIAge")
-"Ceci est un site en développement. Modèle présentement utilisé: wasteClassTEST14"
+"Ceci est un site en développement. Modèle présentement utilisé: wasteClassAD"
 
 localStorage = LocalStorage()
 totalNum = localStorage.getItem("total") if localStorage.getItem("total") else 0
@@ -119,20 +141,7 @@ composted = localStorage.getItem("compost") if localStorage.getItem("compost") e
 trashed = localStorage.getItem("trash") if localStorage.getItem("trash") else 0
 consigned = localStorage.getItem("consigning") if localStorage.getItem("consigning") else 0
 
-with st.expander("Voir les statistiques"):
-    typeCol, numCol = st.columns(spec=2, gap=None, width=500)
-    with typeCol:
-        st.write(":rainbow-background[TOTAL] ")
-        st.write(":blue-background[Recyclage] ")
-        st.write(":orange-background[Compost] ")
-        st.write(":green-background[Contenants consignés] ")
-        st.write(":gray-background[Déchets] ")
-    with numCol:
-        st.write(totalNum)
-        st.write(recycled)
-        st.write(composted)
-        st.write(consigned)
-        st.write(trashed)
+
 
 enable = st.checkbox("Activer la caméra")
 wastePicture_buffer = st.camera_input(key="cameraInput", label="Prenez une photo de l'objet:", disabled=not enable)
@@ -142,27 +151,47 @@ if wastePicture_buffer is not None:
     wasteImage = Image.open(wastePicture_buffer).convert("RGB")
     wasteImage = ImageOps.exif_transpose(wasteImage)
     wasteImage = transform(wasteImage)
-    shownTestImage = v2.functional.to_pil_image(wasteImage)
-    shownTestImage
+    wasteImage = kTransform(wasteImage)
+    _, wasteEdges = canny(wasteImage)
+    wasteImage = wasteEdges.repeat(1, 3, 1, 1)
+    shownTestImage = v2.functional.to_pil_image(wasteImage.squeeze(0))
     with torch.no_grad():
         output = model(wasteImage)
-        _, index = torch.max(output, dim=0)
+        output = torch.softmax(output, dim=1)
+        confidence, index = torch.max(output, dim=1)
+        
         predictedClass = classes[index.item()]
         frPredictedClass = frclasses[index.item()]
+    localStorage.setItem("total", totalNum+1, key='set_total')
+    if predictedClass in ["JuiceBoxLaterals", "JuiceBoxTops", "MilkBoxes", 
+                            "SnackPackages"]:
+        st.header(":blue-background[Recyclage]")
+        localStorage.setItem("recycling", recycled+1, key='set_recycling')
+    elif predictedClass in ["BananaPeels", "UsedPaper"]:
+        st.header(":orange-background[Compost]")
+        localStorage.setItem("compost", composted+1, key='set_compost')
+    elif predictedClass in ["CansLaterals", "CansTops", "JuiceBottlesLaterals",
+                            "JuiceBottlesTops"]:
+        st.header(":green-background[Contenants consignés]")
+        localStorage.setItem("consigning", consigned+1, key='set_consigning')
+    elif predictedClass in ["Shoes"]:
+        st.header(":gray-background[Déchets]")
+        localStorage.setItem("trash", trashed+1, key="set_trash")
+    with st.expander("Voir les détails"):
+        shownTestImage
         st.write("Type d'objet détecté: " + frPredictedClass)
-        localStorage.setItem("total", totalNum+1, key='set_total')
-        if predictedClass in ["AluminumFoil", "Gobelets", "GobeletsLids", 
-                                "JuiceBoxLaterals", "JuiceBoxTops", "MilkBoxes", 
-                                "SandwichPackage", "SnackPackages"]:
-            st.header(":blue-background[Recyclage]")
-            localStorage.setItem("recycling", recycled+1, key='set_recycling')
-        if predictedClass in ["UsedPaper"]:
-            st.header(":orange-background[Compost]")
-            localStorage.setItem("compost", composted+1, key='set_compost')
-        if predictedClass in ["WaterBottlesLaterals", "CansTops", "CansLaterals",
-                                "JuiceBottlesLaterals", "JuiceBottlesTops"]:
-            st.header(":green-background[Contenants consignés]")
-            localStorage.setItem("consigning", consigned+1, key='set_consigning')
-        if predictedClass in ["Shoes"]:
-            st.header(":gray-background[Déchets]")
-            localStorage.setItem("trash", trashed+1, key="set_trash")
+        st.write(f"Certitude: {confidence.item()*100}")
+    with st.expander("Voir les statistiques"):
+        typeCol, numCol = st.columns(spec=2, gap=None, width=350)
+        with typeCol:
+            st.write(":rainbow-background[TOTAL] ")
+            st.write(":blue-background[Recyclage] ")
+            st.write(":orange-background[Compost] ")
+            st.write(":green-background[Contenants consignés] ")
+            st.write(":gray-background[Déchets] ")
+        with numCol:
+            st.write(totalNum)
+            st.write(recycled)
+            st.write(composted)
+            st.write(consigned)
+            st.write(trashed)
