14ef14f35e441c38b05d8e648cb0037c3d749ff9
MMZL256 <138811963+MMZL256@users.noreply.github.com>
2026-03-15 21:41:21 -0400

    Update TrIAgeWebsite.py

--- TrIAgeWebsite.py
+++ TrIAgeWebsite.py
@@ -174,7 +174,7 @@ with st.expander("Voir les statistiques"):
     st.write(":rainbow-background[POINTAGE]", 10*composted + 10*consigned + 5*recycled + trashed)
     st.write("10 points par compostable, 10 points par contenant consigné, 5 points par recyclable, 1 point par déchet")
 with st.expander("Voir votre effet"):
-    "Voici les effets de votre tri:"
+    "Voici les effets de votre tri sur cet appareil:"
     st.write(f":green-background[Argent amassé (contenants consignés)] {consigned*0.1:.2f}$")
     st.write(f":orange-background[Émissions de GES évitées (compost)] {composted*6.5:.2f}g CO2-e")
     st.write(f":blue-background[Émissions de GES évitées (recyclage)] {(recycled+consigned)*24.6:.2f}g CO2-e")
